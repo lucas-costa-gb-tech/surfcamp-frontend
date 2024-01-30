f7a82b137e17f13cf650abda83b6d5c86d1fb7a9
Lucas Alves Costa <109704795+lucas-costa-gb-tech@users.noreply.github.com>
2024-01-30 13:16:54 -0300

    refactor: add barrel files to components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Header } from "@/components/header/component";
-import { Footer } from "@/components/footer/component";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 
 export default function Home() {
   return (
