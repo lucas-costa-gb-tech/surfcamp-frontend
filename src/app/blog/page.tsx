c0392bd2dc6712cb942d042d53820a0ea4e07933
Lucas Alves Costa <109704795+lucas-costa-gb-tech@users.noreply.github.com>
2024-05-18 14:14:08 -0300

    feat: add NewsletterSubscription component to blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,12 @@
 import { FeaturedArticle } from '@/components/featured-article';
+import { NewsletterSubscription } from '@/components/newsletter-subscription';
 
 const Blog = () => {
   return (
     <>
       <main className='blog-page'>
         <FeaturedArticle />
+        <NewsletterSubscription />
       </main>
     </>
   );
