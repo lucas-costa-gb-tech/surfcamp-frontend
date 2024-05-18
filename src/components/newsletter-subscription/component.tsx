822dc4c1734fe016c1e428446ea6d68f62b7968d
Lucas Alves Costa <109704795+lucas-costa-gb-tech@users.noreply.github.com>
2024-05-18 14:52:49 -0300

    feat: add state to NewsletterSubscription component

--- src/components/newsletter-subscription/component.tsx
+++ src/components/newsletter-subscription/component.tsx
@@ -1,12 +1,49 @@
-export const NewsletterSubscription = () => (
-  <section className='newsletter'>
-    <div className='newletter__info'>
-      <h4>subscribe to our newletter</h4>
-      <p className='copy'>Unlock Exclusive Insights and Stay In the Know – Subscribe to Our Newsletter Today to always stay in touch</p>
-    </div>
-    <form className='newsletter__form'>
-      <input type='text' className='newsletter__email input' placeholder='Enter your email address' />
-      <button className='newsletter__subscribe btn btn--medium btn--turquoise'>SUBSCRIBE</button>
-    </form>
-  </section>
-);
+'use client';
+import { useState, type ChangeEvent, type FormEvent } from 'react';
+
+export const NewsletterSubscription = () => {
+  const [email, setEmail] = useState<string>('');
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleNewsletterSubscriptionSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <section className='newsletter'>
+      {/* TODO: Implementar validação de email e envio para o Strapi */}
+      {false ? (
+        <h4 className='newsletter__thanks'>
+          thank you for signing up for the newsletter
+        </h4>
+      ) : (
+        <>
+          <div className='newletter__info'>
+            <h4>subscribe to our newletter</h4>
+            <p className='copy'>Unlock Exclusive Insights and Stay In the Know – Subscribe to Our Newsletter Today to always stay in touch</p>
+          </div>
+          <form
+            className='newsletter__form'
+            onSubmit={handleNewsletterSubscriptionSubmit}
+          >
+            <input
+              type='text'
+              className='newsletter__email input'
+              placeholder='Enter your email address'
+              value={email}
+              onChange={handleEmailChange} />
+            <button
+              type='submit'
+              className='newsletter__subscribe btn btn--medium btn--turquoise'
+            >
+              SUBSCRIBE
+            </button>
+          </form>
+        </>
+      )}
+    </section>
+  );
+};
